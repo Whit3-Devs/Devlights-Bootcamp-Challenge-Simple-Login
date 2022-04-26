19679615f01122badd4be253e664599cb0549049
Augusto Blanco <blanco.augusto.r@gmail.com>
2022-04-25 22:30:29 -0300

    Add background image type svg

---
+++ src/views/Home/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Background from '../../assets/Background'
+
+const Login = () => {
+  return (
+    <>
+        <Background width='100%' height='100%' style={{position: 'absolute', zIndex: '10' }}/>
+    </>
+  )
+}
+
+export default Login
